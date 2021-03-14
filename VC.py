d5393eb6ea77ef6ac653def42851b40fd45cfd71
Ekramul Hassan <78520987+mao2116@users.noreply.github.com>
2021-03-14 19:27:57 +0600

    Add files via upload

---
+++ VC.py
@@ -0,0 +1,108 @@
+# Codder : MD Ekramul Hassan
+# Mao2116
+#FB : https://www.facebook.com/ekramul.hassan.79827
+#Github : https://github.com/mao2116/
+#For Any Kind Of Help Contect With Me IN My FB Inbox
+# -*- coding: utf-8 -*-
+logo= '''
+.------..------..------..------.
+|F.--. ||U.--. ||C.--. ||K.--. |
+| :(): || (\/) || :/\: || :/\: |
+| ()() || :\/: || :\/: || :\/: |
+| '--'F|| '--'U|| '--'C|| '--'K|
+`------'`------'`------'`------'
+'''
+
+
+
+
+
+
+# Here I am Input Same Madule
+from pyfiglet import figlet_format
+from termcolor import colored
+from random import randint
+import os,sys,requests,time
+x = requests.get('https://www.nasa.gov')
+#In This Case I Definded A fanction That  Will Be Ceat A Nice Look To The Print
+def x9 (z):
+  for word in z + '\n':
+     sys.stdout.write(word)
+     sys.stdout.flush()
+     time.sleep(0.01)
+def x5 (z):
+  for word in z + '\n':
+     sys.stdout.write(word)
+     sys.stdout.flush()
+     time.sleep(0.01)
+
+logo1= figlet_format("NaSa Hager", 'isometric2')
+
+c5 = colored(logo, 'red')
+c6 = colored(logo1, 'yellow')
+# Here I Defined Your Name
+n3 =  figlet_format("Story Of", 'slant')
+n5 = figlet_format("A", 'slant')
+n4 = figlet_format("NaSa HaGer", 'slant')
+z1 = colored(n3, 'red')
+z3 = colored(n5, 'yellow')
+z2 = colored(n4, 'cyan')
+x24 = (z1)
+x25 = (z3)
+x26 = (z2)
+x10 = "Thise Tool For Only Fun PerPose"
+x11 = figlet_format(x10, 'slant')
+x12 = colored(x11, 'green')
+x9(x12)
+num = input("Your Name Please :")
+x1 = figlet_format(num, 'slant')
+x2 = colored(x1, 'cyan')
+x5(x2)
+xn = colored(num, 'green')
+ #Bujhe nao!!!
+p = ("Vai Tomare Daiktto Tomare Amra Kano Nabo?😑😑😑")
+p = figlet_format(p, 'digital')
+x6 = colored(p, 'red')
+p1 = "Mao2116"
+x7 = colored(p1, 'red')
+x8 = figlet_format("Now", 'slant')
+x18 = colored(x8, 'green')
+x13 = figlet_format("We", 'slant')
+x19 = colored(x13, 'red')
+x14 = figlet_format("Ready", 'slant')
+x20 = colored(x14, 'cyan')
+x15 = figlet_format("To", 'slant')
+x21 = colored(x15, 'yellow')
+x16 = figlet_format("Go", 'slant')
+x22 = colored(x16, 'blue')
+x9(c5)
+x5(c6)
+while True:
+   x3 = 'y'
+   x4 =input(colored("Iam Not Responded For Any Kind Of Mis Use : y/n :", 'red'))
+   if(x4 == x3):
+    
+    print(colored(x, 'red'))
+    x5(colored(x.text, 'green'))
+    if x.status_code == 200:
+      
+      print(xn, 'We Succes To Do Thise')
+      x5(x18)
+      x5(x19)
+      x5(x20)
+      x5(x21)
+      x5(x22)
+      x5(x2)
+      x5(x24)
+      x5(x25)
+      x5(x26)
+      
+      os.system('xdg-open https://www.nasa.gov')
+    else:
+      print(xn, " We Failled To Do This,, The Site Saw Us 404 Error")
+    break
+    
+   else:
+      x5(x6)
+   
+####Ekramul
